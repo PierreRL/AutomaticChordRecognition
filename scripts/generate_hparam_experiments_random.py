8ca10561bdf283c2f154706cd1c102ba2d3caebb
Pierre Lardet <lardet.pierre@gmail.com>
2025-02-18 18:13:25 +0000

    feat: new hparam searches

--- scripts/generate_hparam_experiments_random.py
+++ scripts/generate_hparam_experiments_random.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Script for generating experiments.txt"""
 import os
+import numpy as np
 
 # The home dir on the node's scratch disk
 USER = os.getenv("USER")
@@ -11,12 +12,18 @@ SCRATCH_HOME = f"{SCRATCH_DISK}/{USER}"
 REPO_HOME = f"/home/{USER}/LeadSheetTranscription"
 DATA_HOME = f"{REPO_HOME}/data/processed"
 
-base_call = f"python {REPO_HOME}/src/run.py --input_dir={DATA_HOME} --output_dir={SCRATCH_HOME}/experiments/lr_search "
+base_call = f"python {REPO_HOME}/src/run.py --input_dir={DATA_HOME} --output_dir={SCRATCH_HOME}/experiments/hparams_random --no_early_stopping"
 
-learning_rates = [1e-5, 1e-4, 1e-3, 1e-2]
-schedulers = ["cosine", "plateau", "none"]
-params = [(lr, sched) for lr in learning_rates for sched in schedulers]
-nr_expts = len(learning_rates) * len(schedulers)
+segment_range = [10, 60]
+layers = [1, 2, 3]
+hidden_size_range = [64, 512]
+nr_expts = 32
+params = []
+for i in range(nr_expts):
+    segment_length = np.random.randint(segment_range[0], segment_range[1])
+    layer = np.random.choice(layers)
+    hidden_size = np.random.randint(hidden_size_range[0], hidden_size_range[1])
+    params.append((segment_length, layer, hidden_size))
 
 nr_servers = 10
 avg_expt_time = 20  # mins
@@ -25,12 +32,15 @@ print(f"Estimated time = {(nr_expts / nr_servers * avg_expt_time)/60} hrs")
 
 output_file = open("./scripts/experiments.txt", "w")
 
-for lr, sched in params:
+for segment_length, layer, hidden_size in params:
     # Note that we don't set a seed for rep - a seed is selected at random
     # and recorded in the output data by the python script
     expt_call = (
         f"{base_call} "
-        f"--lr={lr} --lr_scheduler={sched} --exp_name=lr-search_{sched}_{lr}"
+        f"--segment_length={segment_length} "
+        f"--num_layers={layer} "
+        f"--hidden_size={hidden_size} "
+        f"--exp_name=segment_length_{segment_length}_layers_{layer}_hidden_size_{hidden_size}"
     )
     print(expt_call, file=output_file)
 
