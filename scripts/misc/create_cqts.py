6633f26617998027fa5a49357456b354f33fc174
Pierre Lardet <lardet.pierre@gmail.com>
2025-03-05 16:39:58 +0000

    feat: add create cqts script

---
+++ scripts/misc/create_cqts.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Script for generating experiments.txt"""
+import os
+import numpy as np
+
+# The home dir on the node's scratch disk
+USER = os.getenv("USER")
+# This may need changing to e.g. /disk/scratch_fast depending on the cluster
+SCRATCH_DISK = "/disk/scratch"
+SCRATCH_HOME = f"{SCRATCH_DISK}/{USER}"
+
+REPO_HOME = f"/home/{USER}/LeadSheetTranscription"
+DATA_HOME = f"{REPO_HOME}/data/processed"
+
+base_call = f"python {REPO_HOME}/src/data/create_cached_datasets.py --input_dir={DATA_HOME} --output_dir={DATA_HOME}"
+
+hop_lengths = [512, 1024, 2048, 4096, 8192, 16384]
+nr_expts = len(hop_lengths)
+
+nr_servers = 10
+avg_expt_time = 60  # mins
+print(f"Total experiments = {nr_expts}")
+print(f"Estimated time = {(nr_expts / nr_servers * avg_expt_time)/60} hrs")
+
+output_file = open("./scripts/experiments.txt", "w")
+
+for hop_length in hop_lengths:
+    # Note that we don't set a seed for rep - a seed is selected at random
+    # and recorded in the output data by the python script
+    expt_call = f"{base_call} " f"--hop_length={hop_length} --create_cqts"
+    print(expt_call, file=output_file)
+
+output_file.close()
--- src/data/create_cached_datasets.py
+++ src/data/create_cached_datasets.py
@@ -7,20 +7,25 @@ from src.utils import get_cqt, get_chord_annotation, get_filenames, SR
 from src.data.dataset import FullChordDataset
 
 
-def main(hop_length, create_cqts):
-    dataset = FullChordDataset(hop_length=hop_length)
+def main(
+    hop_length, create_cqts, input_dir="data/processed", output_dir="data/processed"
+):
+    dataset = FullChordDataset(hop_length=hop_length, input_dir=output_dir)
     os.makedirs(dataset.cqt_cache_dir, exist_ok=True)
     os.makedirs(dataset.chord_cache_dir, exist_ok=True)
 
     filenames = get_filenames()
     for filename in tqdm(filenames):
         if create_cqts:
-            cqt = get_cqt(filename, hop_length=dataset.hop_length)
+            cqt = get_cqt(
+                filename, hop_length=dataset.hop_length, override_dir=input_dir
+            )
             torch.save(cqt, f"{dataset.cqt_cache_dir}/{filename}.pt")
 
         chord_ids = get_chord_annotation(
             filename,
             frame_length=hop_length / SR,
+            override_dir=input_dir,
         )
         torch.save(chord_ids, f"{dataset.chord_cache_dir}/{filename}.pt")
 
@@ -32,6 +37,18 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--create_cqts", action="store_true", help="Flag to create CQTs (takes ~1hr)"
+    )
+    parser.add_argument(
+        "--input_dir",
+        type=str,
+        default="data/processed",
+        help="Directory containing the processed data.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="data/processed",
+        help="Directory to save the cached data.",
     )
     args = parser.parse_args()
 
