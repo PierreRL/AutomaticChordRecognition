9bb2a45bc9406b86feecac4b131efe21aef663ee
Pierre Lardet <lardet.pierre@gmail.com>
2025-03-10 15:19:26 +0000

    feat: remove unnecessary script

--- src/run_experiments.sh
+++
@@ -1,9 +0,0 @@
-python src/run.py --exp_name="ismir2017-cr1"
-python src/run.py --exp_name="ismir2017-pitch_shift" --random_pitch_shift
-python src/run.py --exp_name="ismir2017-cr2" --cr2
-python src/run.py --exp_name="ismir2017-lr-0.00001" --lr=0.00001
-python src/run.py --exp_name="ismir2017-lr-0.00005" --lr=0.00005
-python src/run.py --exp_name="ismir2017-lr-0.0001" --lr=0.0001
-python src/run.py --exp_name="ismir2017-lr-0.0005" --lr=0.0005
-python src/run.py --exp_name="ismir2017-lr-0.001" --lr=0.001
-python src/run.py --exp_name="ismir2017-lr-0.005" --lr=0.005
