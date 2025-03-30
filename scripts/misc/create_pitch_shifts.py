98dd52c22c44ddc1910bd82c12e794eeb8e77a4c
Pierre Lardet <lardet.pierre@gmail.com>
2025-03-30 11:07:36 +0100

    again

--- scripts/misc/create_pitch_shifts.py
+++ scripts/misc/create_pitch_shifts.py
@@ -6,7 +6,7 @@ USER = os.getenv("USER")
 
 REPO_HOME = f"/home/{USER}/LeadSheetTranscription"
 
-base_call = f"python {REPO_HOME}/src/data/create_pitch_shifted_audios.py --input_dir={REPO_HOME}/data/processed/audio --output_dir={REPO_HOME}/data/processed/audio --keep_stereo"
+base_call = f"python {REPO_HOME}/src/data/create_pitch_shifted_audios.py --input_dir={REPO_HOME}/data/processed/audio --output_dir={REPO_HOME}/data/processed/audio"
 
 output_file = open("./scripts/experiments.txt", "w")
 
