a4149bf794362adaeda0fc23792333dcc2f5ec50
Pierre Lardet <lardet.pierre@gmail.com>
2025-04-01 11:01:57 +0100

    feat: create shifted ids

---
+++ scripts/misc/create_shifted_chord_ids.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Script for generating experiments.txt"""
+import os
+# import autorootcwd
+USER = os.getenv("USER")
+
+REPO_HOME = f"/home/{USER}/LeadSheetTranscription"
+
+input_dir = f"{REPO_HOME}/data/processed/cache/4096/chords"
+output_dir = f"{REPO_HOME}/data/processed/cache/4096/chords/augs"
+
+os.makedirs(output_dir, exist_ok=True)
+files = os.listdir(input_dir)
+files = [f for f in files if f.endswith(".mp3")]
+max_group_size = 122
+if len(files) % max_group_size != 0:
+    print(
+        f"Warning: {len(files)} files in {input_dir} not divisible by {max_group_size}. "
+        "This may lead to uneven distribution of files across groups."
+    )
+
+
+base_call = f"python src/data/transpose_chord_ids --input_dir={input_dir} --output_dir={output_dir}"
+
+output_file = open("./scripts/experiments.txt", "w")
+
+for i in range(0, len(files), max_group_size):
+    start_idx = i
+    end_idx = min(i + max_group_size, len(files))
+
+    expt_call = (
+        f"{base_call} --start_idx={start_idx} --end_idx={end_idx}"
+    )
+
+    print(expt_call, file=output_file)
+
+output_file.close()
