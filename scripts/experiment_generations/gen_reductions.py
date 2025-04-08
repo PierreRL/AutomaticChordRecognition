26eb78ebeb5d72843c1777e52068808113f33248
Pierre Lardet <lardet.pierre@gmail.com>
2025-04-08 10:41:23 +0100

    add no cqt

--- scripts/experiment_generations/gen_reductions.py
+++ scripts/experiment_generations/gen_reductions.py
@@ -8,7 +8,7 @@ USER = os.getenv("USER")
 REPO_HOME = f"/home/{USER}/LeadSheetTranscription"
 DATA_HOME = f"{REPO_HOME}/data/processed"
 
-base_call = f"python {REPO_HOME}/src/run.py --input_dir={DATA_HOME} --output_dir={REPO_HOME}/experiments/gen_reductions --use_generative_features"
+base_call = f"python {REPO_HOME}/src/run.py --input_dir={DATA_HOME} --output_dir={REPO_HOME}/experiments/gen_reductions --use_generative_features --no_cqt"
 
 gen_reductions = ['avg', 'concat', 'codebook_0' , 'codebook_1', 'codebook_2', 'codebook_3']
 
