011ac8c2477ebc895100ce5c02c1429ca72bff17
Pierre Lardet <lardet.pierre@gmail.com>
2025-04-11 09:20:14 +0100

    added cqt/no_cqt to gen reductions expts

--- scripts/experiment_generations/gen_reductions.py
+++ scripts/experiment_generations/gen_reductions.py
@@ -9,17 +9,27 @@ EDDIE = os.getenv("EDDIE")
 REPO_HOME = f"/home/{USER}/LeadSheetTranscription"
 DATA_HOME = f"{EDDIE}/data/processed"
 
-base_call = f"python {REPO_HOME}/src/run.py --input_dir={DATA_HOME} --output_dir={EDDIE}/experiments/gen_reductions --use_generative_features --no_cqt"
+base_call = f"python {REPO_HOME}/src/run.py --input_dir={DATA_HOME} --output_dir={EDDIE}/experiments/gen_reductions --use_generative_features"
 
-gen_reductions = ['avg', 'concat', 'codebook_0' , 'codebook_1', 'codebook_2', 'codebook_3']
+gen_reductions = [
+    "avg",
+    "concat",
+    "codebook_0",
+    "codebook_1",
+    "codebook_2",
+    "codebook_3",
+]
 
 output_file = open("./scripts/experiments.txt", "w")
 
 for reduction in gen_reductions:
-    # and recorded in the output data by the python script
     expt_call = (
         f"{base_call} --exp_name=reduction_{reduction} --gen_reduction={reduction} "
     )
     print(expt_call, file=output_file)
+
+    expt_call = f"{base_call} --exp_name=reduction_{reduction}_no_cqt --gen_reduction={reduction} --no_cqt "
+    print(expt_call, file=output_file)
+
 
 output_file.close()
