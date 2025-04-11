ffacdd92d07580876945899726722e2ae87a2f30
Pierre Lardet <lardet.pierre@gmail.com>
2025-04-11 18:34:37 +0100

    add batch size option to create gen features

--- src/data/generative_features/create_generative_features.py
+++ src/data/generative_features/create_generative_features.py
@@ -25,6 +25,7 @@ def main(
     start_idx=0,
     end_idx=None,
     max_chunk_length=5,
+    batch_size=16,
 ):
     if output_dir is None:
         output_dir = f"{dir}/cache/{hop_length}/gen-{model_name}"
@@ -60,6 +61,7 @@ def main(
             max_chunk_length=max_chunk_length,
             model=model,
             frame_length=frame_length,
+            max_batch_size=batch_size,
         )
         for reduction, song_repr in song_repr_dict.items():
             os.makedirs(f"{output_dir}/{reduction}", exist_ok=True)
@@ -105,6 +107,12 @@ if __name__ == "__main__":
         help="The length of context in seconds to pass through the model at once. Absolute maximum 30s.",
     )
     parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=16,
+        help="Batch size for processing. Default is 16.",
+    )
+    parser.add_argument(
         "--start_idx",
         type=int,
         required=False,
@@ -126,6 +134,7 @@ if __name__ == "__main__":
         output_dir=args.output_dir,
         model_name=args.model_name,
         max_chunk_length=args.max_chunk_length,
+        batch_size=args.batch_size,
         start_idx=args.start_idx,
         end_idx=args.end_idx,
     )
