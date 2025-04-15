ed4424dac1a06f41250450634b8a566315b6cd49
Pierre Lardet <lardet.pierre@gmail.com>
2025-04-15 09:33:57 +0100

    gen features again

--- scripts/experiment_generations/all_experiments.py
+++ scripts/experiment_generations/all_experiments.py
@@ -204,7 +204,7 @@ for p in probabilities:
     call = f"{base_call} --weight_loss --audio_pitch_shift --cqt_pitch_shift --aug_shift_prob={p} --hmm_smoothing --structured_loss"
     print_to_file(call)
 
-
+"""
 # Generative features
 output_dir = "generative_features"
 model_names = ["large", "small", "chord", "melody", "large-conditioned"]
@@ -214,35 +214,35 @@ for model_name, reduction in [(m, r) for m in model_names for r in reductions]:
     base_call = get_base_call(output_dir, exp_name=exp_name)
     call = f"{base_call} --weight_loss --use_generative_features --gen_model_name={model_name} --gen_reduction={reduction} --hmm_smoothing --structured_loss"
     print_to_file(call)
-"""
+
 
 # Beatwise sampling
-output_dir = "beatwise_sampling"
-exp_name = "none"
-base_call = get_base_call(output_dir, exp_name=exp_name)
-call = f"{base_call} --weight_loss --cqt_pitch_shift --structured_loss"
-print_to_file(call)
-exp_name = "feed_transitions"
-base_call = get_base_call(output_dir, exp_name=exp_name)
-call = f"{base_call} --weight_loss --cqt_pitch_shift --input_transitions --structured_loss"
-print_to_file(call)
+# output_dir = "beatwise_sampling"
+# exp_name = "none"
+# base_call = get_base_call(output_dir, exp_name=exp_name)
+# call = f"{base_call} --weight_loss --cqt_pitch_shift --structured_loss"
+# print_to_file(call)
+# exp_name = "feed_transitions"
+# base_call = get_base_call(output_dir, exp_name=exp_name)
+# call = f"{base_call} --weight_loss --cqt_pitch_shift --input_transitions --structured_loss"
+# print_to_file(call)
 
-beat_intervals = [1/4, 1/2, 1, 2, 4]
-for beat_interval in beat_intervals:
-    exp_name = f"beat_interval_{beat_interval}"
-    base_call = get_base_call(output_dir, exp_name=exp_name)
-    call = f"{base_call} --weight_loss --cqt_pitch_shift --beat_wise_resample --beat_resample_interval={beat_interval} --structured_loss"
-    print_to_file(call)
+# beat_intervals = [1/4, 1/2, 1, 2, 4]
+# for beat_interval in beat_intervals:
+#     exp_name = f"beat_interval_{beat_interval}"
+#     base_call = get_base_call(output_dir, exp_name=exp_name)
+#     call = f"{base_call} --weight_loss --cqt_pitch_shift --beat_wise_resample --beat_resample_interval={beat_interval} --structured_loss"
+#     print_to_file(call)
 
-exp_name = "perfect_beats_train_only"
-base_call = get_base_call(output_dir, exp_name=exp_name)
-call = f"{base_call} --weight_loss --cqt_pitch_shift --beat_wise_resample --perfect_beat_resample --structured_loss"
-print_to_file(call)
+# exp_name = "perfect_beats_train_only"
+# base_call = get_base_call(output_dir, exp_name=exp_name)
+# call = f"{base_call} --weight_loss --cqt_pitch_shift --beat_wise_resample --perfect_beat_resample --structured_loss"
+# print_to_file(call)
 
-exp_name = "perfect_beats_train_and_test"
-base_call = get_base_call(output_dir, exp_name=exp_name)
-call = f"{base_call} --weight_loss --cqt_pitch_shift --cqt_pitch_shift --beat_wise_resample --perfect_beat_resample --perfect_beat_resample_eval --structured_loss"
-print_to_file(call)
+# exp_name = "perfect_beats_train_and_test"
+# base_call = get_base_call(output_dir, exp_name=exp_name)
+# call = f"{base_call} --weight_loss --cqt_pitch_shift --cqt_pitch_shift --beat_wise_resample --perfect_beat_resample --perfect_beat_resample_eval --structured_loss"
+# print_to_file(call)
 
 
 # Print number of experiments in the file
