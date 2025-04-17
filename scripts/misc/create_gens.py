c289dc2b75fa8b97ed8b1168e2fc18587603de8e
Pierre Lardet <lardet.pierre@gmail.com>
2025-04-17 21:14:23 +0100

    fix create gens

--- scripts/misc/create_gens.py
+++ scripts/misc/create_gens.py
@@ -12,7 +12,7 @@ input_dir = f"{DATA_HOME}/audio"
 files = os.listdir(input_dir)
 files = [f for f in files if f.endswith(".mp3")]
 max_group_size = 243
-base_call = f"python {REPO_HOME}/src/data/generative_features/create_generative_features.py --dir={DATA_HOME} --output_dir={DATA_HOME}/cache/4096/gen-large"
+base_call = f"python {REPO_HOME}/src/data/generative_features/create_generative_features.py --dir={DATA_HOME}/audio --output_dir={DATA_HOME}/cache/4096/gen-large"
 
 output_file = open("./scripts/experiments.txt", "w")
 
